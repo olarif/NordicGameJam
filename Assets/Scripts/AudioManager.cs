86d9c71535302311329c3ca073d266b9357d1d0e
olarif <76481740+olarif@users.noreply.github.com>
2022-04-30 10:36:18 +0200

    New Project Upload

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System;
+using UnityEngine.Audio;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    public static AudioManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(this);
+            return;
+        }
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+            s.source.panStereo = s.panStereo;
+        }
+    }
+
+    private void Start()
+    {
+        // Use this to call from any script
+        //FindObjectOfType<AudioManager>().Play("Example Sound");   
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null) return;
+        s.source.Play();
+    }
+}

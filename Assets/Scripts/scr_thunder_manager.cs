b0b2c29271e0bb59508dfb75cf3724ea7b6b8a29
DrewsGameMaker\Drew <2031501hope@gmail.com>
2022-04-30 18:04:53 +0200

    thunder manager script

---
+++ Assets/Scripts/scr_thunder_manager.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_thunder_manager : MonoBehaviour
+{
+    public float maxThunder = 20f;
+    public float minThunder = 10f;
+    public float decreaseThunder = 2f;
+    public float maxLightning = 10f;
+    public float minLightning = 5f;
+    public float decreaseLightning = 1f;
+
+    float thunderTimer;
+    float thunderReset;
+    float lightningTriger;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thunderTimer = maxThunder;
+        lightningTriger = maxLightning;
+        thunderReset = maxThunder;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // decrease time remaining
+       // if 0 trigger thunder
+       // if low trigger warning
+       // reset if 0 but lower to min 
+        thunderTimer -= Time.deltaTime;
+        //Debug.Log(thunderTimer);
+
+        if (thunderTimer < lightningTriger)
+        {
+            Lightning();
+        };
+
+        if (thunderTimer < 0)
+        {
+            if (thunderReset > minThunder)
+            {
+                thunderReset -= decreaseThunder;
+            }
+            if (lightningTriger > minLightning)
+            {
+                lightningTriger -= decreaseLightning;
+            }
+            Thunder();
+            thunderTimer = thunderReset;
+        }
+    }
+
+    public void Thunder()
+    {
+        Debug.Log("Thunder");
+    }
+
+    public void Lightning()
+    {
+        Debug.Log("Lightning" + thunderTimer);
+    }
+}

4305a6f7d3901a83aa317b3439079e06b31960bc
DrewsGameMaker\Drew <2031501hope@gmail.com>
2022-04-30 20:36:59 +0200

    Basic Sounds

--- Assets/Scripts/Tombstone2D.cs
+++ Assets/Scripts/Tombstone2D.cs
@@ -24,7 +24,7 @@ public class Tombstone2D : MonoBehaviour
     void OnTriggerEnter2D(Collider2D col){
 
         if(col.CompareTag("Player")){
-
+            FindObjectOfType<AudioManager>().Play("Ground Transfer 2"); 
             Debug.Log("Collided");
             //player.transform.Find("Player2D").gameObject.transform.position = new Vector3(0,0,0);
             //player.transform.Find("Player3D").gameObject.transform.position = new Vector3(0,0,0);

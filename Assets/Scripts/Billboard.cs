ba7db6c20f308973be0e2ae47cf72523c16bb567
olarif <76481740+olarif@users.noreply.github.com>
2022-04-30 11:48:44 +0200

    updoot

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+
+    Vector3 cameraDir;
+
+    void Update()
+    {
+        cameraDir = Camera.main.transform.forward;
+        cameraDir.y = 0;
+
+        transform.rotation = Quaternion.LookRotation(cameraDir);
+    }
+}

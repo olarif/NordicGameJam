a0ba5d12188680300d69e3e41bae9f3748ee0242
DrewsGameMaker\Drew <2031501hope@gmail.com>
2022-05-01 11:20:02 +0200

    Added Lightning

--- Assets/Scripts/SwitchPerspective.cs
+++ Assets/Scripts/SwitchPerspective.cs
@@ -4,11 +4,28 @@ using UnityEngine;
 
 public class SwitchPerspective : MonoBehaviour
 {
+    public float maxThunder = 20f;
+    public float minThunder = 10f;
+    public float decreaseThunder = 2f;
+    public float maxLightning = 10f;
+    public float minLightning = 5f;
+    public float decreaseLightning = 1f;
+    public GameObject lightningEffect;
+
+    float thunderTimer;
+    float thunderReset;
+    float lightningTriger;
+    float lightningTimerReset = 0.5f;
+    float lightningTimer = 0.5f;
+    bool lightningFlip = true;
+    bool onSwitch = true;
+    bool aboveGround = true;
 
     public Camera mainCam;
 
     public GameObject aboveGroundPlayer;
     public GameObject underGroundPlayer;
+
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +33,11 @@ public class SwitchPerspective : MonoBehaviour
         aboveGroundPlayer.SetActive(true);
          FindObjectOfType<AudioManager>().Play("Rain");
         underGroundPlayer.SetActive(false);
+
+        thunderTimer = maxThunder;
+        lightningTriger = maxLightning;
+        thunderReset = maxThunder;
+
     }
 
     public void GoDown()
@@ -28,6 +50,8 @@ public class SwitchPerspective : MonoBehaviour
         aboveGroundPlayer.SetActive(false);
         underGroundPlayer.SetActive(true);
 
+        aboveGround = false;
+
     }
 
     public void GoUp()
@@ -39,16 +63,114 @@ public class SwitchPerspective : MonoBehaviour
         aboveGroundPlayer.SetActive(true);
         underGroundPlayer.SetActive(false);
 
+        aboveGround = true;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        /*
         if(Input.GetKeyDown(KeyCode.U)){
             GoUp();
         }
         else if (Input.GetKeyDown(KeyCode.I)){
             GoDown();
         }
+        */
+
+           // decrease time remaining
+        // if 0 trigger thunder
+        // if low trigger warning
+        // if 0 trigger thunder
+        // play sound
+        // kill player if above ground
+        // if bellow ground shut off
+        // while shut off if above ground restart
+        
+        if (onSwitch == true)
+        {
+            thunderTimer -= Time.deltaTime;
+            Debug.Log(thunderTimer);
+
+            if (thunderTimer < lightningTriger)
+            {
+                Lightning();
+            };
+
+            if (thunderTimer < 0f)
+            {           
+                Thunder();
+                onSwitch = false;
+            }
+        }
+
+        if (onSwitch == false && aboveGround == true)
+        {
+            ResetTimer();
+        }
+
+
+    }
+
+    public void Thunder()
+    {
+        Debug.Log("Thunder");
+        FindObjectOfType<AudioManager>().Play("Thunder");
+        if (aboveGround == true) 
+        {
+            Debug.Log("Your Dead");
+        }
+        lightningEffect.SetActive(false);
+    }
+
+    public void Lightning()
+    {
+        Debug.Log("Lightning" + thunderTimer);
+        //activate lightning
+        //decrease timer
+        //deactivate timer 
+        if (lightningFlip == true)
+        {
+            if (lightningTimer > 0f)
+            {
+                lightningEffect.SetActive(true);
+                lightningTimer -= Time.deltaTime;
+            }
+            else if (lightningTimer < 0f)
+            {
+                lightningTimer = lightningTimerReset;
+                lightningFlip = false;
+            }
+        }
+        else
+        {
+            if (lightningTimer > 0f)
+            {
+                lightningEffect.SetActive(false);
+                lightningTimer -= Time.deltaTime;
+            }
+            else if (lightningTimer < 0f)
+            {
+                lightningTimer = lightningTimerReset;
+                lightningFlip = true;
+            }
+        }
+
+
+    }
+
+    public void ResetTimer ()
+    {
+        if (thunderReset > minThunder)
+        {
+            thunderReset -= decreaseThunder;
+        }
+        if (lightningTriger > minLightning)
+        {
+            lightningTriger -= decreaseLightning;
+        }
+        thunderTimer = thunderReset;
+        onSwitch = true;
     }
 }
--- Assets/Scripts/scr_thunder_manager.cs
+++ Assets/Scripts/scr_thunder_manager.cs
@@ -10,10 +10,16 @@ public class scr_thunder_manager : MonoBehaviour
     public float maxLightning = 10f;
     public float minLightning = 5f;
     public float decreaseLightning = 1f;
+    public bool aboveGround;
+    public GameObject lightningEffect;
 
     float thunderTimer;
     float thunderReset;
     float lightningTriger;
+    float lightningTimerReset = 0.5f;
+    float lightningTimer = 0.5f;
+    bool lightningFlip = true;
+    bool onSwitch = true;
 
 
 
@@ -30,41 +36,96 @@ public class scr_thunder_manager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       // decrease time remaining
-       // if 0 trigger thunder
-       // if low trigger warning
-       // reset if 0 but lower to min 
-        thunderTimer -= Time.deltaTime;
-        //Debug.Log(thunderTimer);
-
-        if (thunderTimer < lightningTriger)
+        // decrease time remaining
+        // if 0 trigger thunder
+        // if low trigger warning
+        // if 0 trigger thunder
+        // play sound
+        // kill player if above ground
+        // if bellow ground shut off
+        // while shut off if above ground restart
+        
+        if (onSwitch == true)
         {
-            Lightning();
-        };
+            thunderTimer -= Time.deltaTime;
+            //Debug.Log(thunderTimer);
 
-        if (thunderTimer < 0)
-        {
-            if (thunderReset > minThunder)
+            if (thunderTimer < lightningTriger)
             {
-                thunderReset -= decreaseThunder;
+                Lightning();
+            };
+
+            if (thunderTimer < 0f)
+            {           
+                Thunder();
+                onSwitch = false;
             }
-            if (lightningTriger > minLightning)
-            {
-                lightningTriger -= decreaseLightning;
-            }
-            Thunder();
-            thunderTimer = thunderReset;
         }
+
+        if (onSwitch == false && aboveGround == true)
+        {
+            ResetTimer();
+        }
+        
     }
 
     public void Thunder()
     {
         Debug.Log("Thunder");
         FindObjectOfType<AudioManager>().Play("Thunder");
+        if (aboveGround == true) 
+        {
+            Debug.Log("Your Dead");
+        }
     }
 
     public void Lightning()
     {
         Debug.Log("Lightning" + thunderTimer);
+        //activate lightning
+        //decrease timer
+        //deactivate timer 
+        if (lightningFlip == true)
+        {
+            if (lightningTimer > 0f)
+            {
+                lightningEffect.SetActive(true);
+                lightningTimer -= Time.deltaTime;
+            }
+            else if (lightningTimer < 0f)
+            {
+                lightningTimer = lightningTimerReset;
+                lightningFlip = false;
+            }
+        }
+        else
+        {
+            if (lightningTimer > 0f)
+            {
+                lightningEffect.SetActive(false);
+                lightningTimer -= Time.deltaTime;
+            }
+            else if (lightningTimer < 0f)
+            {
+                lightningTimer = lightningTimerReset;
+                lightningFlip = true;
+            }
+        }
+
+
+    }
+
+    public void ResetTimer ()
+    {
+        if (thunderReset > minThunder)
+        {
+            thunderReset -= decreaseThunder;
+        }
+        if (lightningTriger > minLightning)
+        {
+            lightningTriger -= decreaseLightning;
+        }
+        thunderTimer = thunderReset;
     }
 }
+
